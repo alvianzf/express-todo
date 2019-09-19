efc46c048754b189522e3bbe4e2a421b182c2e41
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-09-19 21:56:07 +0700

    put and delete

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -7,4 +7,31 @@ router.get('/', function(req, res) {
     }).catch(err => console.log(err))
 })
 
+router.post('/', (req, res) => {
+    let todos = new Todo(req.body)
+
+    console.log(todos)
+    todos.save( function(err, todo) {
+        if (err) return res.status(422).json({error: err.message})
+
+        return res.status(200).json({result: 'created', todo, err: null})
+    } )
+})
+
+router.put('/:id', (req, res) => {
+    Todo.findOneAndUpdate({_id: req.params.id}, req.body, (err, todo) => {
+        if (err) return res.status(422).json({err})
+
+        return res.status(200).json({result: 'updated', todo, err: null })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Todo.findByIdAndDelete({_id: req.params.id}, (err, todo) => {
+        if (err) return res.status(422).json({err})
+
+        return res.status(200).json({result: 'deleted', todo, err: null})
+    })
+})
+
 module.exports = router
